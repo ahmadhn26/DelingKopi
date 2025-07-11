caa85f4e5db8e70b065e0cd039585d6a20280bad
ahmadhn26 <222312952@stis.ac.id>
2025-07-11 23:34:55 +0700

    update

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'deling_kopi';
-$username = 'root';
-$password = '';
+$dbname = 'projec15_deling_kopi';
+$username = 'projec15_root';
+$password = '@kaesquare123';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
